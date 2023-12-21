6ea36a335fcf197861f25474e76d4915a9a6466a
Горин Пётр <peter14102005@gmail.com>
2023-12-21 13:54:08 +0300

    Just how much do I fix?!

--- src/even.js
+++ src/even.js
@@ -6,12 +6,12 @@ const description = 'Answer "yes" if number is even, otherwise answer "no".';
 const gameEven = () => {
   const ranNumber = randNumber(1, 100);
   const rightAnswer = isEven(ranNumber) ? 'yes' : 'no';
-  const result = {
+  const resultsoftask = {
     answer: rightAnswer,
     question: `${ranNumber}`,
   };
 
-  return result;
+  return resultsoftask;
 };
 
 export default () => gameStart(gameEven, description);
