289455e842c9b5d0131922307c36b53d6e3d66f8
Горин Пётр <peter14102005@gmail.com>
2023-12-21 13:55:24 +0300

    Minor Fix

--- src/even.js
+++ src/even.js
@@ -3,8 +3,8 @@ import gameStart from '../gameStart.js';
 
 const isEven = (question) => question % 2 === 0;
 const description = 'Answer "yes" if number is even, otherwise answer "no".';
+const ranNumber = randNumber(1, 100);
 const gameEven = () => {
-  const ranNumber = randNumber(1, 100);
   const rightAnswer = isEven(ranNumber) ? 'yes' : 'no';
   const resultsoftask = {
     answer: rightAnswer,
