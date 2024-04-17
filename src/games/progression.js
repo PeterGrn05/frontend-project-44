a1e3914c1f043eadf4ff6f6f03ea801d1299b2dc
Горин Пётр <peter14102005@gmail.com>
2024-04-17 10:19:24 +0300

    forgot to readd this.

---
+++ src/games/progression.js
@@ -0,0 +1,31 @@
+import randNumber from '../utils/random.js';
+import gameStart from '../../gameStart.js';
+
+const description = 'What number is missing in the progression?';
+const progressLength = 10;
+
+const gameProgress = () => {
+  const step = randNumber(1, 10);
+  const start = randNumber(1, 99);
+  const hideRightAnswer = randNumber(0, progressLength - 1);
+  const correctAnswer = start + hideRightAnswer * step;
+
+  let progression = '';
+
+  for (let count = 0; count < progressLength; count += 1) {
+    const checkPoint = start + step * count;
+    if (count === hideRightAnswer) {
+      progression += '.. ';
+    } else {
+      progression += `${checkPoint} `;
+    }
+  }
+
+  const result = {
+    answer: String(correctAnswer),
+    question: `${progression}`,
+  };
+  return result;
+};
+
+export default () => gameStart(gameProgress, description);
